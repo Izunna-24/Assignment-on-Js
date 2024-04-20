d6499d537747989cb325e12508bcc0a4a5992c1d
Izunajune <izuchukwuijeudo@gmail.com>
2024-04-20 18:11:02 +0100

    comited

---
+++ nokiaMenu.js
@@ -0,0 +1,46 @@
+function phoneMenu(){
+    console.log("1. Phone Book");
+    console.log("2. Messages");
+    console.log("3. Chat");
+    console.log("4. Call register");
+    console.log("5. Tones");
+    console.log("6. Settings")
+    console.log("7. Call divert")
+    console.log("8. Games")
+    console.log("9. Calculator")
+    console.log("10. Reminders")
+    console.log("11. Clock")
+    console.log("12. Profiles")
+    console.log("13. SIM services");
+}
+
+function showPhoneBook(){
+    console.log("1.Search");
+    console.log("2. Service Nos");
+    console.log("3. Add name");
+    console.log("4. Erase");
+    console.log("5. Edit");
+    console.log("6. Assign tone");
+    console.log("7. Send b'card");
+    console.log("8. Options");
+    console.log('9. Speed dials');
+    console.log('10. Voice tags');
+}
+
+function showOptions(){
+    console.log("1. Type of view");
+    console.log("2. Memory status")
+}
+
+function showMessages(){
+    console.log("1. Write messages");
+    console.log("2. Inbox")
+    console.log("3. Outbox")
+    console.log("4. Picture messages")
+    console.log("5. Templates")
+    console.log("6. Smileys")
+    console.log("7. Message settings")
+    console.log('8. Info service')
+    console.log("9. Voice mailbox number")
+    console.log("10. Service command editor")
+}
