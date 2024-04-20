ca3ca328e9c11d59db12212d129254671b7dbe6b
Izunajune <izuchukwuijeudo@gmail.com>
2024-04-20 03:39:47 +0100

    commit

--- aircondition.js
+++ aircondition.js
@@ -1,1 +1,30 @@
-function
+class AirCondition {
+    constructor() {
+        this.isOn = false;
+        this.temperature = 16;
+    }
+
+    getIsOn() {
+        return this.isOn;
+    }
+
+    turnAcOnAndOff() {
+        this.isOn = !this.isOn;
+    }
+
+    increaseTemperature() {
+        if (this.isOn && this.temperature >= 16 && this.temperature < 30) {
+            this.temperature++;
+        }
+    }
+
+    getTemperature() {
+        return this.temperature;
+    }
+
+    decreaseTemperature() {
+        if (this.isOn && this.temperature > 16 && this.temperature <= 30) {
+            this.temperature--;
+        }
+    }
+}
